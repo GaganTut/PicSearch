6c80c05e51c206ea9a910f60e3ae1c91c6c1836d
GaganTut <gstut@live.com>
2017-04-18 09:54:53 -1000

    Searches and displays images

--- js/app.js
+++ js/app.js
@@ -1,25 +1,49 @@
 /*jshint esversion: 6*/
 
-const searchInputButton = document.querySelector('#searchInputButton');
+const searchInputBox = document.querySelector('#searchInputBox');
 const beginSearchButton = document.querySelector('#beginSearchButton');
+const searchedImages = document.querySelector('#searchedImages');
 
-beginSearchButton.on('click', () => {
-  displayImages(searchImages(searchInputButton.value));
+beginSearchButton.addEventListener('click', () => {
+  searchImages(searchInputBox.value);
 });
 
-const displayImages = (images) => {
+const displayImages = (imageObjects) => {
+  for (let i = 0; i < imageObjects.images.length; i++) {
+    let imageDiv = document.createElement('div');
 
+    let imageTag = document.createElement('img');
+    imageTag.setAttribute('src', imageObjects.images[i].display_sizes[0].uri);
+
+    let imageTitle = document.createElement('h3');
+    imageTitle.innerHTML = imageObjects.images[i].title;
+
+    let imageCaption = document.createElement('p');
+    imageCaption.innerHTML = imageObjects.images[i].caption;
+
+    imageDiv.appendChild(imageTag);
+    imageDiv.appendChild(imageTitle);
+    imageDiv.appendChild(imageCaption);
+
+    searchedImages.appendChild(imageDiv);
+  }
 };
 
 const searchImages = (searchTerm) => {
   const imageSearchRequest = new XMLHttpRequest();
-  imageSearchRequest.on('load', returnImageData);
-  imageSearchRequest.on('error', returnError);
-  imageSearchRequest.setRequestHeader('Api-Key', API_KEY);
+  imageSearchRequest.addEventListener('load', returnImageData);
+  imageSearchRequest.addEventListener('error', returnError);
   imageSearchRequest.open('GET', `https://api.gettyimages.com/v3/search/images?phrase=${searchTerm}`);
+  imageSearchRequest.setRequestHeader('Api-Key', API_KEY);
+  imageSearchRequest.send();
 };
 
 function returnImageData() {
   let imageData = JSON.parse(this.responseText);
   console.log(imageData);
+  displayImages(imageData);
 }
+
+const returnError = () => {
+  console.log('ERROR!!!');
+};
